eebc062e2b19fcbfba280a41ac0b4642e6c6077f
Australiaisfake7 <michaeloverend900@gmail.com>
2026-05-25 15:27:26 +1200

    Added compute shader call

--- main.cpp
+++ main.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <array>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
 
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 
-using std::string;
+using std::string, std::array;
+
+const size_t PARTICLE_COUNT = 1000;
+const float PHYSICS_TIMESTEP = 1.0 / 60.0;
+
+unsigned int currentParticleBuffer = 0;
+double lastFrame = 0.0;
+double dtAccumulator = 0.0;
 
 float vertices[] = {
     -1.0f, 1.0f,
@@ -20,7 +30,29 @@ float vertices[] = {
     1.0f, -1.0f
 };
 
+struct Particle
+{
+    glm::vec2 position;
+    glm::vec2 velocity;
+};
+
 #pragma region Helpers
+
+void physicsUpdate(unsigned int buffers[2], unsigned int program, array<unsigned int, 192> gridCellPointers)
+{
+    glUseProgram(program);
+
+    glUniform1uiv(glGetUniformLocation(program, "gridCellPointers"), 192, gridCellPointers.data());
+
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, buffers[currentParticleBuffer]);
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, buffers[1 - currentParticleBuffer]);
+
+    glDispatchCompute((PARTICLE_COUNT + 63) / 64, 1, 1);
+
+    glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
+
+    currentParticleBuffer = 1 - currentParticleBuffer;
+}
 
 string readFile(const char* path)
 {
@@ -146,6 +178,13 @@ int main()
 
     deleteShaders(shaders, sizeof(shaders) / sizeof(unsigned int));
 
+    string particleShaderSource = readFile("src/shaders/particle.comp");
+
+    unsigned int particleShader = createShader(particleShaderSource.c_str(), GL_COMPUTE_SHADER);
+    unsigned int particleShaderProgram = createShaderProgram(&particleShader, 1);
+
+    deleteShaders(&particleShader, 1);
+
     unsigned int VAO, VBO;
 
     glGenVertexArrays(1, &VAO);
@@ -162,9 +201,29 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
+    unsigned int particleBuffers[2];
+
+    glGenBuffers(2, particleBuffers);
+
+    array<Particle, PARTICLE_COUNT> particles;
+    array<unsigned int, 192> gridCellPointers;
+
+    glBindBuffer(GL_SHADER_STORAGE_BUFFER, particleBuffers[0]);
+    glBufferData(GL_SHADER_STORAGE_BUFFER, sizeof(Particle) * PARTICLE_COUNT, particles.data(), GL_DYNAMIC_DRAW);
+    glBindBuffer(GL_SHADER_STORAGE_BUFFER, particleBuffers[1]);
+    glBufferData(GL_SHADER_STORAGE_BUFFER, sizeof(Particle) * PARTICLE_COUNT, nullptr, GL_DYNAMIC_DRAW);
+
+    lastFrame = glfwGetTime();
+
     // This is the render loop
     while (!glfwWindowShouldClose(window))
     {
+        double now = glfwGetTime();
+        double deltatime = now - lastFrame;
+        lastFrame = now;
+
+        dtAccumulator += deltatime;
+
         glfwPollEvents();
         ProcessInput(window);
 
@@ -174,6 +233,12 @@ int main()
 
         glClearColor(1.00f, 0.49f, 0.04f, 1.00f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        while (dtAccumulator >= PHYSICS_TIMESTEP)
+        {
+            physicsUpdate(particleBuffers, particleShaderProgram, gridCellPointers);
+            dtAccumulator -= PHYSICS_TIMESTEP;
+        }
 
         glUseProgram(shaderProgram);
 
